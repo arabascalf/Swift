8da8851494276dba9cfd2d7dfa87e63101d3ffe8
Aranzza Abascal <arabascalf@gmail.com>
2017-10-05 21:29:43 -0500

    mini reto - semana 2

---
+++ miniReto.playground/Contents.swift
@@ -0,0 +1,41 @@
+//Semana 2: Mini Reto
+
+/*
+ 
+ Crear una serie del 0 al 100 (incliur ambos elementos)
+ Si el número es divisible entre 5: imprimir #Bingo!!!
+ Si el número es par: #par
+ Si el número es impar: #impar
+ Si el número está entre 30 y 40: #Viva Swift!!!
+ 
+*/
+
+import UIKit
+
+/*
+ Ciclo for-each para recorrer los elementos, no pude realizar un for tradicional porque no supe cómo hacerlo en Swift 3
+ 
+ 
+ https://github.com/arabascalf/Swift.git
+ f7ca378ac5b97e11f4900e1e8656a766d29c8aeb
+*/
+for i in 0...100 {
+    
+    if (i % 2 == 0){
+        print("\(i) par")
+    }
+    
+    if (i % 2 != 0){
+        print("\(i) impar")
+    }
+    
+    if (i % 5 == 0){
+        print("\(i) Bingo!!!")
+    }
+    
+    if (i >= 30 && i <= 40){
+        print("\(i) Viva Swift!!!")
+    }
+    
+    print("\n")
+}
